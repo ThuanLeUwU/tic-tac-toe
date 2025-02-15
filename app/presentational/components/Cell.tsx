297f1a2511e75ddbf50ab7e2fa7939ab8fe20765
Thuanlee <lthuan2809@gmail.com>
2025-02-16 02:02:15 +0700

    change-div-to-span

--- app/presentational/components/Cell.tsx
+++ app/presentational/components/Cell.tsx
@@ -10,12 +10,18 @@ type CellProps = {
 const Cell: React.FC<CellProps> = ({ value, onClick }) => {
   return (
     <div
-      className={`cell ${classNames({
-        "tw-text-blue-600": value === Cells.X,
-        "tw-text-red-600": value === Cells.O,
-      })}`}
+      className="cell tw-flex tw-items-center tw-justify-center"
       onClick={onClick}>
-      {value === Cells.Empty ? null : value}
+      {value !== Cells.Empty && (
+        <span
+          className={classNames("tw-font-bold tw-text-center", {
+            "tw-text-blue-600 tw-text-[8vw] md:tw-text-[5vw]":
+              value === Cells.X, // X màu xanh
+            "tw-text-red-600 tw-text-[8vw] md:tw-text-[5vw]": value === Cells.O, // O màu đỏ
+          })}>
+          {value}
+        </span>
+      )}
     </div>
   );
 };
